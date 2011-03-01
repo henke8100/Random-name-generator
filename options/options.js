ece30b121f0bdd39d2615792fc481e0c7b3a08ef
Henrik <henrik@HenrikDatorUbuntu.(none)>
2011-03-01 21:33:19 +0100

    Updated style for options

--- options/options.js
+++ options/options.js
@@ -1,3 +1,5 @@
+var current="tab1";
+
 $(document).ready(function(){
 	$(".menuitem").mouseover(function (){
 		$(this).css('background', 'rgba(150,150,150,0.5)');
@@ -5,4 +7,16 @@ $(document).ready(function(){
 	$(".menuitem").mouseout(function (){
 		$(this).css('background', 'rgba(150,150,150,0.2)');
 	});
+	$(".menuitem").click(function (){
+		$(this).css('background', 'rgba(150,150,150,0.8)');
+		page(this);
+	});
 });
+
+function page(t){
+	if(t.id!=current){
+		$("#"+current+"c").slideUp();
+		$("#"+t.id+"c").slideDown();
+		current=t.id;
+	}
+}
